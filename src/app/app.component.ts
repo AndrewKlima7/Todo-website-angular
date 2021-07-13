99fffbe63cf92f42b15020139920a4f67fe85304
Andrew Klima <andrewklima7@gmail.com>
2021-07-13 16:20:02 -0500

    Website with css sheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Todo } from './todo';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo';
+
+  todos: Todo[] = [{task: "fold clothes",completed: false},
+  {task: "put clothes away",completed: false},
+  {task: "relax",completed: false},
+  {task: "try to pet cat",completed: true},
+  {task: "pet dog",completed: false},
+  {task: "be awesome",completed: false}]
+
+  addTodo(form: NgForm){
+    let task = form.form.value.task;
+    let completed = false;
+    let newTodo: Todo = {task: task, completed: completed};
+    this.todos.push(newTodo); 
+  }
+
+  completeTodo(task: number){
+    for(let i = 0; i < this.todos.length; i++){
+      let todo: Todo = this.todos[task];
+      todo.completed = true;
+    }
+
+  }
+
+  removeTodo(task: number){
+      this.todos.splice(task, 1);
+  }
 }
